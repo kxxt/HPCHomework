d2e4a4ddcd1949979690f78b8e59f60807ba5f25
kxxt <rsworktech@outlook.com>
2021-11-13 18:07:45 +0800

    Save mat-mul-vec.cpp.

--- mat-mul-vec.cpp
+++ mat-mul-vec.cpp
@@ -3,7 +3,6 @@
 #include "mpi.h"
 #include<cstdio>
 #include<iostream>
-#include<algorithm>
 
 using namespace std;
 
@@ -23,22 +22,19 @@ get_mat_and_vec_from_input(int mat[MAX_BUFFER_HEIGHT * MAX_BUFFER_WIDTH], int ve
     for (int i = 0; i < n; i++) scanf("%d", &vec[i]);
 }
 
-inline int inner_product(const int **arr1, const int **arr2, int l) {
+inline int inner_product(const int *arr1, const int *arr2, int l) {
     int sum = 0;
-    for (int i = 0; i < l; i++) {
-        sum += *arr1[i] * *arr2[i];
-    }
+    for (int i = 0; i < l; i++) sum += arr1[i] * arr2[i];
     return sum;
 }
 
 inline void pretty_print_vec(const int *vec, int l) {
-    putchar('[');
-    putchar(' ');
+    cout << "[";
     for (int i = 0; i < l; i++) {
-        printf("%d ", vec[i]);
+        printf("%d", vec[i]);
+        if (i!=l-1) cout << ", ";
     }
-    putchar(']');
-    putchar('\n');
+    cout << "]" << endl;
 }
 
 int main(int argc, char **argv) {
@@ -49,24 +45,37 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     // Code
+    // 这里我直接像 OI 里常做的那样把数组大小写死了，当然也可以动态申请内存。
     int m, n, mat[MAX_BUFFER_HEIGHT * MAX_BUFFER_WIDTH], vec[MAX_BUFFER_HEIGHT];
     if (rank == 0) {
+        // 获取用户输入
         get_demensions(&m, &n);
         get_mat_and_vec_from_input(mat, vec, m, n);
-        int *tags = new int[size]; // 存储 tag 状态
-        fill(tags, tags + size, 0); // 数组初始化为 0
-        for (int i = 0; i < m; i++) {
-            int tar = m % size;
-            MPI_Send(&mat[n * i], n, MPI_INT, tar, tags[tar]++, MPI_COMM_WORLD);
-        }
-        delete[] tags;
     }
+    // 广播所有信息
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(vec, n, MPI_INT, 0, MPI_COMM_WORLD);
-    pretty_print_vec(vec, n);
-    int ntags = m / size + 1;
+    MPI_Bcast(mat, n * m, MPI_INT, 0, MPI_COMM_WORLD);
+    // pretty_print_vec(vec, n);
+    MPI_Request *requests = new MPI_Request[m / size + 1];
+    int cur = 0;
+    for (int i = rank; i <= m; i += size) {
+        // cout << i << endl;
+        int prod = inner_product(&mat[i * m], vec, n);
+        MPI_Isend(&prod, 1, MPI_INT, 0, i, MPI_COMM_WORLD, &requests[cur++]); // 异步发送，不阻塞循环
+    }
+    if (rank == 0) {
+        int result[MAX_BUFFER_HEIGHT];
+        for (int i = 0; i < m; i++) {
+            // cout << "recv " << i << endl;
+            MPI_Recv(&result[i], 1, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);
+        }
+        pretty_print_vec(result, n);
+    }
+    for (int i = 0; i < cur; i++)MPI_Wait(requests + i, &status);
     // Finalize
+    delete[] requests; // 释放资源
     MPI_Finalize();
     return 0;
 }
