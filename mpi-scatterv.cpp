82be850b85b913d13cb7b93e96006d516d6a9940
kxxt <rsworktech@outlook.com>
2021-11-27 09:58:07 +0800

    Color output in mpi-scatterv.cpp.

--- mpi-scatterv.cpp
+++ mpi-scatterv.cpp
@@ -7,6 +7,10 @@
 #include<cstdlib>
 #include<ctime>
 
+#define COLOR_START "\x1B[3"
+#define COLOR_END   "m"
+#define RESET "\x1B[0m"
+
 int main(int argc, char **argv) {
     // Initialize
     srand(time(NULL));
@@ -32,17 +36,29 @@ int main(int argc, char **argv) {
         for (int i = 0; i < size; ++i) printf("%d ", displs[i]);
         puts("");
         puts("Rank 0: Initializing data...\nThe data is shown as below:");
-        for (int i = 0; i < datacnt; i++) {
-            data[i] = rand();
-            printf("%.8x ", data[i]);
-            if (i % 8 == 7) puts("");
+        int cnt = 0;
+        for (int i = 0; i < size; i++) {
+            printf(COLOR_START "%d" COLOR_END, i % 7 + 1);
+            for (int j = 0; j < i + 1; j++, cnt++) {
+                data[cnt] = rand();
+                printf("%.8x ", data[cnt]);
+                if (cnt % 9 == 8)printf("\n");
+            }
+            printf(RESET);
         }
-        if (size % 8 != 0) puts("");
+//        for (int i = 0; i < datacnt; i++) {
+//            data[i] = rand();
+//            printf("%.8x ", data[i]);
+//            if (i % 8 == 7) puts("");
+//        }
+        if (size % 9 != 0) puts("");
         puts("Rank 0: Scatterv data...");
     }
     MPI_Scatterv(data, sendcnts, displs, MPI_INT, buf, rank + 1, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("Rank %d: Received ", rank);
+    printf("Rank %.2d: Received ", rank);
+    printf(COLOR_START "%d" COLOR_END, rank % 7 + 1);
     for (int i = 0; i < rank + 1; i++) printf("%.8x ", buf[i]);
+    printf(RESET);
     puts("");
     // MPI_Barrier(MPI_COMM_WORLD);
     // Finalize
