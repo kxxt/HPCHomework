316d0d3b2b247f699089ccef75ec3ca500328b0a
kxxt <rsworktech@outlook.com>
2021-12-04 09:10:39 +0800

    Add omp-schedule-guided.cpp

---
+++ omp-schedule-guided.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kxxt on 2021/12/4.
+//
+#include <omp.h>
+#include <cstdio>
+
+int main() {
+#pragma omp parallel for schedule(guided) default(none)
+    for (int i = 0; i < 10; i++) {
+        printf("thread %d, iteration %d\n", omp_get_thread_num(), i);
+    }
+    return 0;
+}
